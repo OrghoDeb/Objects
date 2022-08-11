4121288eb9981564f3f25730ced190de85b0d1a1
OrghoDeb <orghodeb@gmail.com>
2022-08-11 21:27:51 +0600

    The objects code doesn't work

--- JsChallengerObjects.js
+++ JsChallengerObjects.js
@@ -28,14 +28,36 @@ function objectChecker(obj, cond) {
 }
 console.log(objectChecker({ a: 1, b: undefined, c: 3 }, "b"));
 
-console.log("\nTask 5 = Creating Javascript objects one with one argument");
-function objCreator(f) {
-  return Object.create(f);
-}
-console.log(objCreator("a"));
+// console.log("\nTask 5 = Creating Javascript objects one with one argument");
+// function objCreator(f) {
+//   return Object.create(f);
+// }
+// console.log(objCreator("a"));
 
 console.log("\nTask 6 = Sum object values");
-function valueSummation(...x) {
-  return Object.values(...x).sum();
+function valueSummation(x) {
+  let b = Object.values(x);
+  return b.add();
 }
 console.log(valueSummation({ a: 1, b: 2, c: 3 }));
+
+// console.log("\nTask 7 = Creating Javascript objects two");
+// function objectCreator2(a, b) {
+//   return { a: `${b}` };
+// }
+// console.log(objCreator2("a", "b"));
+// console.log("\nTask 8 = Sum object values");
+// function ObjectSum(a) {
+//   return;
+// }
+// console.log(`\nTask 9 = Extracting Infos from objects`);
+// function myFunction(obj) {
+//   return {
+//     ["size"]: obj.size.toString().concat("cm"),
+//     ["weight"]: obj.weight.toString().concat("kg"),
+//     ...obj,
+//   };
+// }
+// console.log(
+//   myFunction({ fn: "Lisa", ln: "Müller", age: 17, size: 175, weight: 67 })
+// );
